3da3b378b523913c76c3f90d569af30cdce9d87c
Dmitry PATRIKEEV <dmitry.patrikeev.ext@opencascade.com>
2021-09-25 23:24:47 +0300

    modules

---
+++ main.js
@@ -0,0 +1,30 @@
+class UserTable extends HTMLElement {
+    constructor() { super(); /* ... */ }
+    connectedCallback() { /* ... */ }
+    disconnectedCallback() { /* ... */  }
+    static get observedAttributes() { return [/* ... */]; }
+    attributeChangedCallback(name, oldValue, newValue) { /* ... */ }
+    adoptedCallback() { /* ... */ }
+}
+customElements.define('user-table', MyElement);
+
+class TableFilter extends HTMLElement {
+    constructor() { super(); /* ... */ }
+    connectedCallback() { /* ... */ }
+    disconnectedCallback() { /* ... */  }
+    static get observedAttributes() { return [/* ... */]; }
+    attributeChangedCallback(name, oldValue, newValue) { /* ... */ }
+    adoptedCallback() { /* ... */ }
+}
+customElements.define('table-filer', MyElement);
+
+class LoadingSpinner extends HTMLElement {
+    constructor() { super(); /* ... */ }
+    connectedCallback() { /* ... */ }
+    disconnectedCallback() { /* ... */  }
+    static get observedAttributes() { return [/* ... */]; }
+    attributeChangedCallback(name, oldValue, newValue) { /* ... */ }
+    adoptedCallback() { /* ... */ }
+}
+customElements.define('loading-spinner', MyElement);
+
